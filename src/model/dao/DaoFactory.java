1d1a2aca92ee03586671b206f42f76e253199a65
johnnyubiali <johnnyubiali@hotmail.com>
2022-07-25 16:35:10 -0300

    SellerList

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -6,7 +6,7 @@ import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
-	public static SellerDao createSellerDAo() {
+	public static SellerDao createSellerDao() {
 	return new SellerDaoJDBC(DB.getConnection());
 	}
 	
